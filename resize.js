8d51faea19f31a8ea062e69f566195b529a0655a
mkberg0917 <mkberg0917@gmail.com>
2022-01-25 13:56:54 -0500

    Update resize.js

--- resize.js
+++ resize.js
@@ -4,6 +4,18 @@ const widthOutput = document.querySelector('#width');
 function resize() {
   heightOutput.textContent = window.innerHeight;
   widthOutput.textContent = window.innerWidth;
+  
+  
+  var tall = window.innerHeight;
+  var wide = window.innerWidth;
+  
+
+  
+  document.getElementByClassName("tabcontent").innerHTML = "Paragraph changed!" + tall;
+  
+  
+  
+  
 }
 
 window.onresize = resize;
