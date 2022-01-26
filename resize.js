ed9850df607afd0ef203f4cdaa04194e46d6d0ea
mkberg0917 <mkberg0917@gmail.com>
2022-01-26 14:34:32 -0500

    Update resize.js

--- resize.js
+++ resize.js
@@ -1,37 +1,24 @@
-function resize() {
-  
-  var tall = window.innerHeight;
-  var wide = window.innerWidth;
-  var frameh='';
-  var framew='';
-  
-  if (tall > 800) {
-      frameh = '540';
-      } else if (tall > 600) {
-      frameh = '480';
-      } else if (tall > 400) {
-      frameh = '320';
-      } else {
-      frameh = '280';
-      }
-  
-    if (wide > 800) {
-      framew = '540';
-      } else if (wide > 600) {
-      framew = '480';
-      } else if (wide > 400) {
-      framew = '320';
-      } else {
-      framew = '280';
-      }
+//Resize Facebook feed
+$(window).on('resize', function() {
+  setTimeout(function() {
+    changeFBPagePlugin()
+  }, 500);
+});
 
-  
-  document.getElementById("Corporation").innerHTML = '<div target="_top" class="fb-page" data-href="https://www.facebook.com/tiptonschools/" data-tabs="timeline" data-width="' + framew + '" data-height="' + frameh + '" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false" data-lazy="true"><blockquote cite="https://www.facebook.com/TiptonElement/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/TiptonElement/">Tipton Elementary School</a></blockquote></div>';
-  document.getElementById("Elementary").innerHTML = '<div target="_top" class="fb-page" data-href="https://www.facebook.com/tiptonschools/" data-tabs="timeline" data-width="'+framew+'" data-height="'+frameh+'" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false" data-lazy="true"><blockquote cite="https://www.facebook.com/TiptonElement/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/TiptonElement/">Tipton Elementary School</a></blockquote></div>';
-  document.getElementById("Middle").innerHTML = '<div target="_top" class="fb-page" data-href="https://www.facebook.com/tiptonschools/" data-tabs="timeline" data-width="'+framew+'" data-height="'+frameh+'" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false" data-lazy="true"><blockquote cite="https://www.facebook.com/TiptonElement/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/TiptonElement/">Tipton Elementary School</a></blockquote></div>';
-  document.getElementById("High").innerHTML = '<div target="_top" class="fb-page" data-href="https://www.facebook.com/tiptonschools/" data-tabs="timeline" data-width="'+framew+'" data-height="'+frameh+'" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false" data-lazy="true"><blockquote cite="https://www.facebook.com/TiptonElement/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/TiptonElement/">Tipton Elementary School</a></blockquote></div>';
-  
-}
+$(window).on('load', function() {
+  setTimeout(function() {
+    changeFBPagePlugin()
+  }, 1500);
+});
 
-window.onload = resize;
+changeFBPagePlugin = function() {
+  var container_width = Number($('.fb-container').width()).toFixed(0);
+  var container_height = Number($('.fb-container').height()).toFixed(0);
+  if (!isNaN(container_width) && !isNaN(container_height)) {
+    $(".fb-page").attr("data-width", container_width).attr("data-height", container_height);
+  }
+  if (typeof FB !== 'undefined') {
+    FB.XFBML.parse();
+  }
+};
 
